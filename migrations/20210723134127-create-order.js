3ff704f0a05cd0eb15f99021380778dd0c598493
Ruby Li <r844312@gmail.com>
2021-07-23 21:58:02 +0800

    Add sequelize and create Order, OrderItem and Product model

---
+++ migrations/20210723134127-create-order.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      productId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ models/order.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Order.belongsToMany(models.Product, {
+        through: models.OrderItem,
+        foreignKey: 'OrderId',
+        as: 'items'
+      });
+    }
+  };
+  Order.init({
+    amount: DataTypes.INTEGER,
+    status: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING,
+    productId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
