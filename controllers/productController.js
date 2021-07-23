dc8141d2aed8eef1945d59b1d98ebde393fcab6d
Ruby Li <r844312@gmail.com>
2021-07-23 22:31:26 +0800

    Add product listing API

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,34 @@
+const { image } = require('faker');
 const db = require('../models');
 const { Product } = db;
 
 module.exports = {
+  getProducts: async(req, res) => {
+    try {
+      let products = await Product.findAll({
+        raw: true,
+        nest: true,
+        attributes: ['name', 'price', 'image']
+      });
+      return res.json({
+        code: 200,
+        data: products
+      });
+    }
+    catch (err) {
+      console.error(`getProducts fail, ${err.message}`);
+    }
+  },
+
   getProduct: async(req, res) => {
     try {
       let { id } = req.params;
-      let product = await Product.findByPk(id);
+      let product = await Product.findOne({
+        where: {
+          id: id
+        },
+        attributes: ['name', 'price', 'image', 'description']
+      });
       if (!product) {
         return res.json({
           code: 400,
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController')
+const productController = require('../controllers/productController');
 
+router.get('/products', productController.getProducts);
 router.get('/product/:id', productController.getProduct);
 
 module.exports = router;
